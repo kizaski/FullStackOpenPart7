70ab60a7a1880a955deb997d10527b8bbfe38b89
Kiril <kiril2315@gmail.com>
2024-04-11 15:29:07 +0300

    add and use clsx for notification conditional styling

--- bloglist/frontend/src/components/Notification.jsx
+++ bloglist/frontend/src/components/Notification.jsx
@@ -1,8 +1,12 @@
+import clsx from 'clsx'
+
 const Notification = ({ message, type }) => {
-  const notifStyles =
-    type === 'info'
-      ? 'bg-lightgray text-green-600 italic border border-solid border-green-600 rounded-lg p-4 mb-4'
-      : 'bg-lightgray text-red-600 border border-solid border-red-600 rounded-lg p-4 mb-4'
+  const notifStyles = clsx({
+    'bg-lightgray text-green-600 italic border border-solid border-green-600 rounded-lg p-4 mb-4':
+      type === 'info',
+    'bg-lightgray text-red-600 border border-solid border-red-600 rounded-lg p-4 mb-4':
+      type === 'error',
+  })
   if (!message) return null
   return <div className={notifStyles}>{message}</div>
 }
